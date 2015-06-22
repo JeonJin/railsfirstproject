ead843ae1bd8f86410c905ccceae014fdbcd00d8
jjkang <z700zzz@gmail.com>
2015-06-22 21:58:08 +0900

    git

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.developent?
+	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png" 
 	else
 
